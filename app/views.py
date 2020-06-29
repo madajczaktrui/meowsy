8d158602f60441be9828524cfa6c9b1bbca43768
Jakub Madajczak <madajczak@trui.pl>
2020-06-29 12:11:21 +0200

    api consume & dev env

--- app/views.py
+++ app/views.py
@@ -6,8 +6,8 @@ import os
 
 @app.route('/')
 def home():
-    #host = os.environ.get('API_HOST', 'kittini')
-    #port = os.environ.get('API_PORT', '5001')
-    #with urllib.request.urlopen(host + ':' + port) as url:
-        #data = json.loads(url.read().decode())
-    return render_template('index.html')#, data)
+    host = os.environ.get('API_HOST', 'kittini')
+    port = os.environ.get('API_PORT', '5001')
+    with urllib.request.urlopen('http://' + host + ':' + port) as url:
+        data = json.loads(url.read().decode())
+    return render_template('index.html', gangster = data)
